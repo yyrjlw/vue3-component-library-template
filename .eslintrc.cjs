d50b65b81b78acc13881481047a1266ae9c285da
豌豆射手 <lwxingkong@qq.com>
2024-03-29 18:27:09 +0800

    移除auto-import插件

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,15 +7,13 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting',
-    './.eslintrc-auto-import.json'
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
   rules: {
     'vue/multi-word-component-names': 'off',
-    'vue/no-mutating-props': 'warn',
-    'no-undef': 'warn'
+    'vue/no-mutating-props': 'warn'
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import cssInjectedByJs from 'vite-plugin-css-injected-by-js'
 import { resolve } from 'node:path'
-import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    AutoImport({
-      dts: true,
-      imports: ['vue'],
-      eslintrc: {
-        enabled: true
-      }
-    }),
-    cssInjectedByJs()
-  ],
+  plugins: [vue(), vueJsx(), cssInjectedByJs()],
   resolve: {
     alias: {}
   },
