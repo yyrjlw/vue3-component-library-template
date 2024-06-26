46702fd3fe961cda1d038456403e0a23fd30fe03
豌豆射手 <lwxingkong@qq.com>
2024-06-26 16:57:20 +0800

    自动添加css类前缀插件忽略uno.css库

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ export default {
   plugins: {
     'postcss-prefix-selector': {
       prefix: '.vue3-tmp', //css类前缀，推荐使用你的库名
-      ignoreFiles: ['node_modules']
+      ignoreFiles: [/node_modules/, /uno\.css/]
     }
   }
 }
